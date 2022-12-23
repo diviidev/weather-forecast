c446ed3f5c543151f916185e801a6bbe737f2c74
diviidev <divyanshut4214@gmail.com>
2022-12-24 03:01:34 +0530

    this is my final weather forecast website

---
+++ javascript/index.js
@@ -0,0 +1,46 @@
+function getDate() {
+  var date = new Date().toLocaleDateString();
+  var x = document.getElementById("head2");
+  x.innerHTML = `${date}`;
+  console.log(date);
+}
+function weather() {
+  var parent = document.getElementById("column1");
+  var city = document.getElementById("searchbox").value;
+  var key = "ecfae499d3a6990d513e872644874cd4";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}&units=metric%60`;
+
+  fetch(url) //here we call the api/request
+    .then((response) => response.json()) //after the api is fetched what should be done ->we converted the backend response to json format
+    .then((data) => {
+      console.log(data);
+      var div = document.createElement("div");
+      div.classList.add("showbox");
+      // var icon = `https://openweathermap.org/img/wn/&{}.png`;
+      div.innerHTML = `
+      <div class="left">
+        <p class="tempshow ">
+        ${Math.round(data.main.temp)} °C
+        </p>
+        <img src="https://openweathermap.org/img/wn/${
+          data.weather[0].icon
+        }.png" alt="CA"/>
+      </div>
+      <div class="right"> 
+        <p class="weathershow">
+        Weather : ${data.weather[0].main}
+        </p>  
+        <p class="humidityshow">
+        Humidity : ${Math.round(data.main.humidity)}
+        </p>
+        <p class="windshow">
+        Wind speed: ${data.wind.speed}
+        </p>
+      </div>
+      `;
+      parent.appendChild(div);
+    });
+}
+
+// ${Math.round(
+//   data.main.temp
